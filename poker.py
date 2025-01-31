7ebd89db266df056954db5b460f49765aee62d97
mgeilinger <milesgeilinger@gmail.com>
2025-01-31 17:38:38 +0000

    Kickers added
    Have improved the functions to provide kickers when necessary.

--- poker.py
+++ poker.py
@@ -4,6 +4,10 @@ suits = ['H', 'D', 'C', 'S']
 ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 two_pair_1=[('K', 'H'), ('K', 'C'), ('4', 'C'), ('8', 'C'), ('8', 'H')]
+high_card_1=[('4', 'H'), ('7', 'C'), ('2', 'C'), ('K', 'C'), ('8', 'H')]
+three_1=[('3', 'S'), ('3', 'C'), ('K', 'D'), ('Q', 'D'), ('3', 'H')]
+three_2=[('K', 'S'), ('K', 'C'), ('3', 'D'), ('5', 'D'), ('K', 'H')]
+four_1=[('3', 'S'), ('3', 'C'), ('2', 'D'), ('3', 'D'), ('3', 'H')]
 full_house_1=[('K', 'H'), ('K', 'D'), ('K', 'C'), ('5', 'C'), ('5', 'H')]
 pair_1=[('K', 'H'), ('6', 'C'), ('4', 'C'), ('8', 'C'), ('8', 'H')]
 straight_1=[('K', 'H'), ('10', 'C'), ('J', 'C'), ('A', 'C'), ('Q', 'H')]
@@ -50,10 +54,11 @@ def card_count(cards, num, but=None):
             return value
     return False
 
-def high_card(cards):
+def high_card(cards, but=None):
     dist = hand_dist(cards)
-    result = (hc,k1,k2,k3,k4)
-    return result
+    if not isinstance(but, set):
+        but = {but}
+    return max(value(card) for card in cards if value(card) not in but)
 
 def is_flush(cards):
     dist = hand_dist(cards)
@@ -80,30 +85,61 @@ def is_straight_flush(cards):
     return False
 
 def is_four(cards):
-    return card_count(cards,4)
+    hc = card_count(cards, 4)
+    k = high_card(cards, hc)
+    if hc is not False:
+        return [hc,k]
+    return False
 
 def is_three(cards):
-    return card_count(cards,3)
+    hc = card_count(cards, 3)
+    k1 = high_card(cards, hc)
+    k2 = high_card(cards, {hc,k1})
+    if hc is not False:
+        return [hc,k1,k2]
+    return False
 
 def is_pair(cards):
-    return card_count(cards,2)
+    hc = card_count(cards, 2)
+    k1 = high_card(cards, hc)
+    k2 = high_card(cards, {hc,k1})
+    k3 = high_card(cards, {hc,k1,k2})
+    if hc is not False:
+        return [hc,k1,k2,k3]
+    return False
 
-def is__two_pair(cards):
-    hc = card_count(cards,2)
-    k1 = card_count(cards,2,hc)
-    if (k1 is not False):
-        return [hc,k1]
+def is_two_pair(cards):
+    hc1 = card_count(cards,2)
+    hc2 = card_count(cards,2,hc1)
+    k = high_card(cards, {hc1,hc2})
+    if hc1<hc2:
+        hc1, hc2 = hc2, hc1
+    if (hc2 is not False):
+        return [hc1,hc2,k]
     return False
 
 def is_full_house(cards):
     hc = card_count(cards,3)
-    k1 = card_count(cards,2,hc)
-    if (k1 is not False):
-        return [hc,k1]
+    k = card_count(cards,2,hc)
+    if (k is not False):
+        return [hc,k]
     return False
+
+def is_high_card(cards):
+    hc = high_card(cards)
+    k1 = high_card(cards,hc)
+    k2 = high_card(cards,{hc,k1})
+    k3 = high_card(cards,{hc,k1,k2})
+    k4 = high_card(cards,{hc,k1,k2,k3})
+    return [hc,k1,k2,k3,k4]
 
 # def hand_rank(cards):
 #     if
+
+# print('Should be: 13: ', high_card(pair_1))
+# print('Should be: 14: ', high_card(straight_1))
+# print('Should be: 13: ', high_card(straight_1,14))
+# print('Should be: 12: ', high_card(straight_1,{14,13}))
 
 # print(is_straight(pair_1))
 # print(is_straight_flush(royal_flush_1))
@@ -121,6 +157,17 @@ def is_full_house(cards):
 
 # print(is_full_house(full_house_1))
 # print(is_full_house(straight_1))
+
+# print(is_four(full_house_1))
+# print(is_four(four_1))
+
+# print(is_three(three_1))
+# print(is_three(three_2))
+# print(is_three(straight_1))
+
+print(is_high_card(straight_1))
+print(is_high_card(straight_2))
+print(is_high_card(high_card_1))
 
 # print(is_straight(straight_3))
 
