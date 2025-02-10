523491664426ea8ac2edeaa406bada3919e10cf0
mgeilinger <milesgeilinger@gmail.com>
2025-02-10 17:20:08 +0000

    Improved three of a kind
    Previous version of three of a kind assumed that all cards being discarded were unique and that held cards and discarded cards had no rank the same. This was done as this is the optimal way to discard for the best hand.
    
    However, I've now accounted for all cases.

--- prob.py
+++ prob.py
@@ -87,6 +87,46 @@ def three_probability(cards, states):
     draws = states.count(True)
     if draws == 0: # Don't calculate if you're not discarding anything
         return 0
+    kept_ranks = kept_card_ranks(cards, states)
+    winners = can_you_make_n_of_a_kind(kept_ranks,3,draws)
+    discarded_ranks = discarded_card_ranks(cards, states)
+    print('kept ranks:',kept_ranks)
+    print('discarded ranks:',discarded_ranks)
+    new_prob = 0
+    old_prob = 0
+    disc_prob = 0
+    if (draws>=3):
+        for rank in discarded_ranks:
+            print('rank is:',rank)
+            # Check occurences in original hand
+            seen = 4 - kept_ranks.count(rank) - discarded_ranks.count(rank)
+            N = deck
+            n = draws
+            k = 3
+            print('N is:',deck)
+            print('K is:',seen)
+            print('n is:',n)
+            print('k is:',k)
+            disc_prob += pmf(N,seen,n,k)
+        new_prob += disc_prob + 8*pmf(deck,4,draws,3)
+    for winner in winners:
+        N = deck
+        # Check occurences of winner in discarded cards and subtract those from K
+        discarded_winner = discarded_ranks.count(winner[0])
+        K = 4 - len(winner) - discarded_winner
+        n = draws
+        k = 3 - len(winner)
+        old_prob += pmf(N,K,n,k)
+    probability = new_prob + old_prob
+    return round(100*probability,2)
+
+def pair_probability(cards, states):
+    if is_four(cards) is not False and is_three(cards) is not False and is_pair(cards) is not False: # Don't calculate if you already have a four of a kind or three of a kind
+        return None
+    deck = 47
+    draws = states.count(True)
+    if draws == 0: # Don't calculate if you're not discarding anything
+        return 0
     new_prob = 0
     old_prob = 0
     if (draws>=3):
@@ -99,8 +139,8 @@ def three_probability(cards, states):
         n = draws
         k = 3 - len(winner)
         old_prob += pmf(N,K,n,k)
-    print(old_prob)
-    print(new_prob)
+    # print(old_prob)
+    # print(new_prob)
     probability = new_prob + old_prob
     return round(100*probability,2)
 
@@ -146,7 +186,7 @@ hand = generate_hand()
 formatted_hand = format_poker_hand(hand)
 
 s = [False, True, True , True, True]
-c = [('5', 'H'), ('J', 'H'), ('6', 'H'), ('3', 'H'), ('4', 'H')]
+c = [('5', 'H'), ('5', 'H'), ('4', 'H'), ('7', 'H'), ('J', 'H')]
 
 # print('The probability of drawing a straight is:',straight_probability(c,s),'%')
 # print('The probability of drawing a flush is:',flush_probability(c,s),'%')
